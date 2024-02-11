bee29b7a5be58ce218a98bd02c9c89be5c227fe0
julia-b <butkovajulia@gmail.com>
2024-02-11 10:01:37 +0100

    bug fixes

--- learning_plan.py
+++ learning_plan.py
@@ -85,10 +85,12 @@ class LearningPlan:
         next_exercise_word_id = next_exercise_data['id'].item()
         next_word_num_reps = next_exercise_data['num_reps'].item()
 
+        user_data = self.user_config.get_user_data(chat_id)
+        show_test_metrics = False if 'show_test_metrics' not in user_data.keys() else user_data['show_test_metrics']
         user_level = self.user_config.get_user_data(chat_id)['level']
         if 'test' == mode:
             exercise = WordsExerciseTest(word=next_exercise_word, word_id=next_exercise_word_id, lang=lang, level=user_level,
-                                         add_metrics=self.user_config.get_user_data(chat_id)['show_test_metrics'])
+                                         add_metrics=show_test_metrics)
         else:
             exercise = WordsExerciseLearn(word=next_exercise_word, word_id=next_exercise_word_id, lang=lang, num_reps=next_word_num_reps)
 
