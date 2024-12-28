46a1d861489af94171e0b38ebe4965cce6d3c904
julia-b <butkovajulia@gmail.com>
2024-12-28 16:31:18 +0100

    minor update

--- learning_plan.py
+++ learning_plan.py
@@ -14,98 +14,6 @@ from utils import get_assistant_response
 from words_exercise import FlashcardExercise, WordsExerciseLearn, WordsExerciseTest
 
 
-class _LearningPlan:
-    def __init__(self, interface, templates, words_progress_db=None, words_db=None, decks_db=None, user_config=None):
-        self.progress_db = words_progress_db
-        self.words_db = words_db
-        self.decks_db = decks_db
-        self.user_config = user_config
-        self.interface = interface
-        self.templates = templates
-
-        # Words that are repeated at least this many times will be used for testing exercises,
-        # others will be used for learning exercises.
-        self.test_threshold = 2
-
-    def get_next_words_exercise(self, chat_id, lang, mode):
-
-        words_df = self.words_db.get_words_df()
-
-        # choose words from the deck
-        current_deck_id = self.user_config.get_user_data(chat_id)['current_deck_id']
-        deck_words_df = words_df.loc[words_df['id'].isin(self.decks_db.get_deck_words(current_deck_id))]
-
-        if deck_words_df.shape[0] == 0:
-            return None
-
-        progress_df = self.progress_db.get_progress_df()
-        progress_words_df = pd.merge(progress_df, deck_words_df, how='outer', left_on='word_id',
-                                     right_on='id', sort=False)
-        not_ignored_words = progress_words_df.loc[(progress_words_df['lang'] == lang.lower()) &
-                                                  progress_words_df['to_ignore'].isin([False, np.nan])]
-
-        test_words_mask = not_ignored_words['num_reps'] >= self.test_threshold
-        test_words = not_ignored_words.loc[test_words_mask]
-
-        test_weights = [max(1/v[1], 0.001) for v in test_words['num_reps'].items()]
-
-        not_ignored_learn_words = not_ignored_words[~test_words_mask]
-        seen_words = not_ignored_learn_words.loc[~not_ignored_learn_words['num_reps'].isna()]
-
-        if seen_words.shape[0] > 3:
-            learn_words = seen_words
-        else:
-            learn_words = not_ignored_learn_words
-
-        learn_weights = [1] * len(learn_words)
-
-        if 'test' == mode:
-            words = test_words
-            weights = test_weights
-        elif 'learn' == mode:
-            words = learn_words
-            weights = learn_weights
-        elif ('repeat_test' == mode) or ('repeat_learn' == mode):
-            # choose not ignored words that have been seen least often
-            min_num_reps = not_ignored_words['num_reps'].min()
-            words = not_ignored_words.loc[not_ignored_words['num_reps'] == min_num_reps]
-            weights = [1] * len(words)
-        else:
-            return None
-
-        if words.shape[0] == 0:
-            # there are no words for the selected mode
-            print('Warning: There are 0 selected words.')
-            return None
-
-        next_exercise_data = words.sample(n=1, weights=np.array(weights))
-        next_exercise_word = next_exercise_data['word'].item()
-        next_exercise_word_id = next_exercise_data['id'].item()
-        next_exercise_word_meaning = next_exercise_data['meaning'].item()
-        next_word_num_reps = next_exercise_data['num_reps'].item()
-
-        user_data = self.user_config.get_user_data(chat_id)
-        show_test_metrics = False if 'show_test_metrics' not in user_data.keys() else user_data['show_test_metrics']
-        user_level = self.user_config.get_user_data(chat_id)['level']
-        uilang = self.user_config.get_user_ui_lang(chat_id)
-        if 'test' == mode:
-            exercise = WordsExerciseTest(word=next_exercise_word, word_id=next_exercise_word_id, lang=lang, uilang=uilang, level=user_level, interface=self.interface, templates=self.templates,
-                                         add_metrics=show_test_metrics)
-        else:
-            exercise = WordsExerciseLearn(word=next_exercise_word, meaning=next_exercise_word_meaning,
-                                          word_id=next_exercise_word_id, lang=lang, uilang=uilang, num_reps=next_word_num_reps, interface=self.interface,
-                                          templates=self.templates)
-
-        return exercise
-
-    # def get_next_exercise_like(self, exercise, chat_id, lang, mode):
-    #     if isinstance(exercise, WordsExerciseLearn) or isinstance(exercise, WordsExerciseTest):
-    #         next_exercise = self.get_next_words_exercise(chat_id, lang, mode)
-    #     else:
-    #         raise ValueError(f'Non-word exercises are not supported yet')
-    #     return next_exercise
-
-
 class LearningPlan:
     def __init__(self, interface, templates, words_progress_db=None, words_db=None, decks_db=None, user_config=None):
         self.progress_db = words_progress_db
@@ -134,9 +42,7 @@ class LearningPlan:
 
         words_df = self.words_db.get_words_df()
 
-        user_data = self.user_config.get_user_data(chat_id)
-        current_deck_id = user_data['current_deck_id']
-        deck_words_df = words_df.loc[words_df['id'].isin(self.decks_db.get_deck_words(current_deck_id))]
+        deck_words_df = words_df
 
         if deck_words_df.shape[0] == 0:
             return None
@@ -256,16 +162,17 @@ class LearningPlan:
         uilang = user_data['ui_language']
 
         words_df = self.words_db.get_words_df()
-        words_lang = words_df[words_df['lang'] == lang]
 
-        decks_df = self.decks_db.get_decks_df()
-        user_decks = decks_df[decks_df['owner'] == str(chat_id)]['id']
-        user_words = []
-        for deck in user_decks:
-            deck_words = words_lang.loc[words_lang['id'].isin(self.decks_db.get_deck_words(deck))]['word'].to_list()
-            user_words += deck_words
+        if words_df.shape[0] == 0:
+            return None
 
-        user_words_str = ', '.join(user_words)
+        progress_df = self.progress_db.get_progress_df()
+        progress_words_df = pd.merge(progress_df, words_df, how='outer', left_on='word_id',
+                                     right_on='id', sort=False)
+        not_ignored_words = progress_words_df.loc[(progress_words_df['lang'] == lang.lower()) & (progress_words_df['chat_id'] == chat_id) &
+                                                  progress_words_df['to_ignore'].isin([False, np.nan])]
+
+        user_words_str = ', '.join(not_ignored_words['word'].to_list())
 
         message_template = self.templates[uilang]['gen_words'] if 'gen_words' in self.templates[uilang].keys() else \
                             self.templates[uilang][lang]['gen_words']
